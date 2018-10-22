b4bc868398d114717631ac33bfe2d905055afa94
naoki-ishii <kyoumi155@gmail.com>
2018-10-22 23:26:36 +0900

    initial commit

---
+++ app/src/main/java/com/iiinaiii/breweries/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.iiinaiii.breweries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
